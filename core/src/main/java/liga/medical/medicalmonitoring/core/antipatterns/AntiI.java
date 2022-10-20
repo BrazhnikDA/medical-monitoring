308433b4277e15e446429a5849fe757ddeb9a90a
DESKTOP-KEAG5H6\Dima <dimitry.brazhnik@yandex.ru>
2022-10-20 14:33:52 +0300

    impl

---
+++ core/src/main/java/liga/medical/medicalmonitoring/core/antipatterns/AntiI.java
@@ -0,0 +1,10 @@
+package liga.medical.medicalmonitoring.core.antipatterns;
+
+public class AntiI {
+    interface allFunction {
+        void getWidth();
+        void buyPencil();
+        void playWithDog();
+        void goToTheMoon();
+    }
+}
